f1aabf9e6ee672f5397b8d0556e39717b823dfa5
UrosZikic <uroszikic1512988@gmail.com>
2023-09-21 11:29:00 +0200

    graxpo site init

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var menu = document.querySelector(".menu-function");
+var navMenu = document.querySelector(".nav-menu");
+var link = document.querySelectorAll(".link-js");
+var count = 0;
+
+menu.onclick = () => {
+  if (count % 2 == 0) {
+    navMenu.style.visibility = "visible";
+    document.body.style.overflow = "hidden";
+  } else {
+    navMenu.style.visibility = "hidden";
+    document.body.style.overflow = "scroll";
+  }
+  ++count;
+};
+link.forEach(
+  (item) =>
+    (item.onclick = () => {
+      if (window.screen.width <= 1216) {
+        navMenu.style.visibility = "hidden";
+        document.body.style.overflow = "scroll";
+      }
+    })
+);
